8fddc2e7276917218f0e52581239212c321bdc29
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-11-18 12:56:46 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,23 @@
+let x, y, Xadder, Yadder;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
+    x = width/2;
+    y = height/2;
+    adder = 1;
 }
 
 function draw() {
     background(220);
+    circle(x ,y ,50, 50);
+    x = x + Xadder;
+    if (x > width || x < 0){
+        Xadder *= -1;
+    }
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    y = y + Yadder;
+    if (y > height || y < 0){
+        Yadder *= -1;
+    }
 }
